1659df9788541072c9541663ee30ee2b6ebff065
StefanDenis08 <denis10david25@gmail.com>
2023-12-16 14:49:26 +0200

    First Commit

---
+++ motivational-videos-module.js
@@ -0,0 +1,7 @@
+import StefanAPI from '../../../api/out/stefanAPI.js'
+
+export async function main() {
+    console.log('motivational-videos-module')
+}
+
+if (process.argv.includes('--test')) await main()
---
+++ test.ps1
@@ -0,0 +1,4 @@
+Clear-Host
+& node motivational-videos-module.js --test
+Pause
+Clear-Host
