d57e74152c88a14d91c088dd7b11e6db3dd63f0b
StefanDenis08 <denis10david25@gmail.com>
2023-12-16 21:41:31 +0200

    Updated code so that it opens browser on start

--- start-module.js
+++ start-module.js
@@ -1,7 +1,9 @@
 import Main from './out/motivational-videos-module.js'
+import { spawnSync } from 'child_process'
 
 export async function main() {
     await Main()
 }
 
 if (process.argv.includes('--test')) await main()
+if (!process.argv.includes('--test')) spawnSync('cmd', ['/c', 'start', 'http://localhost:491'])
