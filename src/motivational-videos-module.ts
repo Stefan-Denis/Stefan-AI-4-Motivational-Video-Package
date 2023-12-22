b6c9fad5908dd167d0e1c0c6d5d82a040596c35b
StefanDenis08 <denis10david25@gmail.com>
2023-12-22 18:17:47 +0200

    added a route to start the video production

--- src/motivational-videos-module.ts
+++ src/motivational-videos-module.ts
@@ -15,6 +15,7 @@ import StefanAPI from '../../../../api/out/stefanAPI.js'
 /**
  * NPM Import
  */
+import { spawnSync } from 'child_process'
 import bodyparser from 'body-parser'
 import { fileURLToPath } from 'url'
 import express from 'express'
@@ -26,10 +27,6 @@ import path from 'path'
  */
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-/**
- * Routes
- */
-
 export default async function Main() {
     const app: express.Application = express()
 
@@ -40,15 +37,58 @@ export default async function Main() {
         res.sendFile(path.join(__dirname, '../ui/index.html'))
     })
 
+    /**
+     * ? Videos
+     */
     app.get('/videos/list', (req, res) => {
         res.json(fs.readdirSync(path.join(__dirname, '../storage/videos')).filter((element) => element.endsWith('.mp4')))
     })
 
     app.get('/video/display', function (req: express.Request, res: express.Response) {
-        const elementName = req.query.elementName as string
-        res.sendFile(path.join(__dirname, `../storage/videos/${elementName}`))
+        res.sendFile(path.join(__dirname, `../storage/videos/${req.query.elementName}`))
     })
 
+    app.post('/videos/setorder', (req, res) => {
+        fs.writeFileSync(path.join(__dirname, '../storage/config/clipsOrder.json'), JSON.stringify(req.body, null, 4)); res.sendStatus(200)
+    })
+
+    app.get('/open/fs', (req, res) => {
+        spawnSync('explorer', [path.join(__dirname, '../storage/videos')]); res.sendStatus(200)
+    })
+
+    /**
+     * ? Script
+     */
+    app.get('/script/getScript', (req, res) => {
+        res.json(fs.readFileSync(path.join(__dirname, '../storage/config/videoScript.txt'), 'utf-8'))
+    })
+
+    app.post('/script/setScript', (req, res) => {
+        fs.writeFileSync(path.join(__dirname, '../storage/config/videoScript.txt'), req.body.text); res.sendStatus(200)
+    })
+
+    /**
+     * ? Subtitles
+     */
+    app.get('/subtitles/getstyle', (req, res) => {
+        res.json(fs.readFileSync(path.join(__dirname, '../storage/config/defaultSubtitles.ass'), 'utf-8'))
+    })
+
+    app.post('/subtitles/setstyle', (req, res) => {
+        fs.writeFileSync(path.join(__dirname, '../storage/config/defaultSubtitles.ass'), req.body.text); res.send(200)
+    })
+
+    /**
+     * ? Start Production
+     */
+    app.get('/start', (req, res) => {
+        const powershell = spawnSync('powershell.exe', ['.\\start.ps1', '--nonIntegratedTerminal'], { cwd: path.join(__dirname, '../cli') })
+        res.sendStatus(200)
+    })
+
+    /**
+     * ? App Listen
+     */
     app.listen(491, () => {
         console.log('Server started on port 491')
         console.log('http://localhost:491')
